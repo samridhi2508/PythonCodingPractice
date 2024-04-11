989f3c23d683fd108071c357419ddeca2bcfee0c
Samridhi Mishra <samridhi2508@gmail.com>
2024-04-11 14:28:42 +0530

    Palindrome and Reverse string

---
+++ Strings/ReverseString.py
@@ -0,0 +1,7 @@
+def reverse_string(s):
+    return s[::-1]
+
+# Test the function
+input_string = input("Enter a string: ")
+reversed_string = reverse_string(input_string)
+print("Reversed string:", reversed_string)
