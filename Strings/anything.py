cc0559fe80fd4e3020c2ca211ef58620ff32b208
Samridhi Mishra <samridhi2508@gmail.com>
2024-04-18 00:07:35 +0530

    word counter program to count the words in a given text
    word counter

---
+++ Strings/anything.py
@@ -0,0 +1,24 @@
+import turtle
+t= turtle.Turtle()
+turtle.Screen().bgcolor("black")
+
+t.penup()
+t.goto(-230,175)
+t.pendown()
+
+t.pencolor("white")
+t.pensize(15)
+t.forward(100)
+t.back(50)
+t.right(90)
+t.forward(170)
+t.right(90)
+t.forward(50)
+t.backward(100)
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+
+
